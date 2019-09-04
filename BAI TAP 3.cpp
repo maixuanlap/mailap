9199b88d5a0fedc20bf5094bc23a884d535de713
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2019-09-04 23:34:39 +0700

    bài tập

---
+++ BAI TAP 3.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int a;
+	int b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	if(a>b){
+	printf("gia tri so lon hon a=%d\n",a);
+	}else{
+		printf("gia tri lon hon b= %d\n,b");
+	}
+	return 0;
+	}
---
+++ BAI TAP 4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("nhap vao gia tri a");
+	scanf("%d",&a);
+	if (a>0){
+	printf("%d la so duong",a);
+	}else{
+		printf("%d la so am",a);
+	}
+	
+	return 0;
+	}
