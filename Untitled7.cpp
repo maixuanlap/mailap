f1ffd7300123d2cbeaf83d0f7adbfb63303ecb70
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2019-09-06 22:23:40 +0700

    bai tap ve nhà 6.9

---
+++ Untitled7.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+	
+	for(;n>=1 && n<=3;){
+		  if(n==1){
+		printf("chon cafe\n");
+		}else if(n==2){
+			printf("chon sinh to\n");
+			}else if(n==3){
+				printf("chon nuoc ep\n");
+				
+				
+				}
+					}
+					return 0;
+					}
