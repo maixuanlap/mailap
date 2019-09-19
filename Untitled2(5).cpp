4c45981bba6e6598e1a2dde2131278231bd9b9f3
maixuanlap <54585739+maixuanlap@users.noreply.github.com>
2019-09-19 16:21:48 +0700

    Add files via upload

---
+++ Untitled2(5).cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+void lowerCase(char x[]){
+	printf("Mai Lap");
+	scanf("%d",&x);
+	for(int i=0;i<50;i++)
+	{
+		if(s[i]>=65&&s[i]<=90){
+			printf("nhap");
+			}else{
+				printf("%C",%x);
+			}
+	
+	}
+	int main(){
+		char[50];
+		lowerCase(x);
+		
+		}
+
